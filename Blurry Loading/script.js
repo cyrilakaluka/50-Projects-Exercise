f5bcad4088b4a4b8c366295d2dc1d6931e2db7b3
Akaluka Chibueze Cyril <akalukacyril@gmail.com>
2020-12-28 21:39:26 +0200

    Added Blurry Loading Project

---
+++ Blurry Loading/script.js
@@ -0,0 +1,18 @@
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+
+let load = 0;
+
+let intId = setInterval(blurring, 30);
+
+function blurring() {
+  load++;
+
+  if (load > 99) {
+    clearInterval(intId);
+  }
+
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = `${1 - load / 100}`;
+  bg.style.filter = `blur(${30 - (load / 100) * 30}px)`;
+}
